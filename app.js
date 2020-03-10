f35854266cb39574c3dc5d6b5cbc16342d7b52e1
Sergey Novikov <nsv47nsv36@yandex.ru>
2020-03-11 00:24:42 +0300

    add csv

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 let express = require('express')
 const axios = require('axios')
+const FS = require('fs')
 let app = express()
 
 var nodeHtmlParser = require("node-html-parser")
+
+// import * as FS from 'fs'
 
 axios.get('http://luis-lpa.ru/catalog/sirens-ceiling/lpa-10n.html')
     .then(function(response){
@@ -10,7 +13,10 @@ axios.get('http://luis-lpa.ru/catalog/sirens-ceiling/lpa-10n.html')
         const result = nodeHtmlParser.parse(response.data);
         let arrRes = result.querySelectorAll('tr')
         for(let i = 0; i < arrRes.length; ++i){ 
-            console.log(result.querySelectorAll('tr')[i].structuredText);
+            // console.log(result.querySelectorAll('tr')[i].structuredText);
+            // FS.writeFileSync("some.csv", exampleData.map(row=> `${row.name}${separator}${row.age}`).join("\n"))
+            FS.appendFileSync("some.csv", result.querySelectorAll('tr')[i].structuredText+';');
+            
         }
     })
 
