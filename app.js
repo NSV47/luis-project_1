09ac83932b43cff7d6f5b0c9e9fb1e57b4c2c5b8
Sergey Novikov <nsv47nsv36@yandex.ru>
2020-03-11 15:47:09 +0300

    проход по первому товару

--- app.js
+++ app.js
@@ -6,37 +6,31 @@ let app = express()
 var nodeHtmlParser = require("node-html-parser")
 
 axios.get('http://luis-lpa.ru/catalog').then(function(response){
-    const result = nodeHtmlParser.parse(response.data);
-    // console.log(result.querySelector('.catalog').classNames.toString() === 'container,catalog')
-    // console.log(result.querySelector('.container.catalog').classNames.toString().firstChild.structure)
-    // console.log(result.querySelector('.container.catalog').structuredText)
-    // if(result.querySelector('.container.catalog').classNames.toString() === 'container,catalog'){
-    //     console.log(result.querySelector('.container.catalog'))
-    // }
-    // console.log(result.querySelector('.catalog').querySelectorAll('.col')[1].text)
-    let arrObject = result.querySelector('.catalog').querySelectorAll('.object')
-    console.log(arrObject[0].structuredText)
-    console.log(arrObject[0].querySelector('a').getAttribute('href'))
-    for(let i = 0; i < arrObject.length; ++i){
-        
-    }
-})    
-
-// axios.get('http://luis-lpa.ru/catalog/sirens-ceiling/lpa-10n.html')
-//     .then(function(response){
-//         // console.log(response);
-//         const result = nodeHtmlParser.parse(response.data);
-//         let arrRes = result.querySelectorAll('tr')
-//         for(let i = 0; i < arrRes.length; ++i){ 
-//             // console.log(result.querySelectorAll('tr')[i].structuredText);
-//             // FS.writeFileSync("some.csv", exampleData.map(row=> `${row.name}${separator}${row.age}`).join("\n"))
-//             FS.appendFileSync("some.csv", result.querySelectorAll('tr')[i].structuredText+';');
-            
-//         }
-//     })
+    const resultCatalog = nodeHtmlParser.parse(response.data);
+    let arrCatalog = resultCatalog.querySelector('.catalog').querySelectorAll('.object')
+    console.log(arrCatalog[0].structuredText)
+    console.log(arrCatalog[0].querySelector('a').getAttribute('href'))
+    axios.get('http://luis-lpa.ru'+arrCatalog[0].querySelector('a').getAttribute('href')).then(function(response){
+        const resultFunctionalGroupOfProducts = nodeHtmlParser.parse(response.data) // notification and evacuation management system 
+        let arrFunctionalGroupOfProducts = resultFunctionalGroupOfProducts.querySelector('.catalog').querySelectorAll('.object')
+        console.log(arrFunctionalGroupOfProducts[0].structuredText)
+        console.log(arrFunctionalGroupOfProducts[0].querySelector('a').getAttribute('href'))
+        axios.get('http://luis-lpa.ru'+arrFunctionalGroupOfProducts[0].querySelector('a').getAttribute('href')).then(function(response){
+            const resultProductsGroup = nodeHtmlParser.parse(response.data);
+            let arrProductsGroup = resultProductsGroup.querySelector('.catalog').querySelectorAll('.object')
+            console.log(arrProductsGroup[0].structuredText)
+            console.log(arrProductsGroup[0].querySelector('a').getAttribute('href'))
+            axios.get('http://luis-lpa.ru'+arrProductsGroup[0].querySelector('a').getAttribute('href')).then(function(response){
+                const resultProducts = nodeHtmlParser.parse(response.data)
+                let arrSpecifications = resultProducts.querySelectorAll('tr')
+                for(let i = 1; i < arrSpecifications.length; ++i){
+                    console.log(resultProducts.querySelectorAll('tr')[i].structuredText)
+                }
+            })
+        })
+    })
+})
 
 app.listen(3000, function(){
     console.log('запущено на localhost:3000')
 })
-
-return 0
