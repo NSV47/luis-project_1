7451c88218ab13333092d9f461a466b096a3e67c
Sergey Novikov <nsv47nsv36@yandex.ru>
2020-03-15 23:55:02 +0300

    спсок всех товаров

--- app.js
+++ app.js
@@ -37,52 +37,42 @@ myGet(baseUrl+'/catalog').then((arr) => {
         promises.push(myGet(baseUrl+arr[i]))
     }
     Promise.all(promises).then(responses => {
-        console.log(responses[0].length)
+        // console.log(responses)
+        let promises = []
+        for(let j = 0; j < responses.length; ++j){
+            for(let k = 0; k < responses[j].length; ++k){
+                // console.log(responses[j][k])
+                if(responses[j][k].endsWith('.html')){
+                    console.log(responses[j][k])
+                    fs.appendFileSync('some.csv', responses[j][k]+'\n')
+                }else{
+                    promises.push(myGet(baseUrl+responses[j][k]))
+                }
+            }
+        }
+        Promise.all(promises).then(responses => {
+            // console.log(responses)
+            let promises = []
+            for(let j = 0; j < responses.length; ++j){
+                for(let k = 0; k < responses[j].length; ++k){
+                    // console.log(responses[j][k])
+                    if(responses[j][k].endsWith('.html')){
+                        console.log(responses[j][k])
+                        fs.appendFileSync('some.csv', responses[j][k]+'\n')
+                    }else{
+                        promises.push(myGet(baseUrl+responses[j][k]))
+                    }
+                }
+            }
+            Promise.all(promises).then(responses => {
+                // console.log(responses)
+                for(let j = 0; j < responses.length; ++j){
+                    for(let k = 0; k < responses[j].length; ++k){
+                        console.log(responses[j][k])
+                        fs.appendFileSync('some.csv', responses[j][k]+'\n')
+                    }
+                }
+            })
+        })
     })
 })
-
-
-
-axios.get('http://luis-lpa.ru/catalog/soue/').then(function(response){
-    const resultFunctionalGroupOfProducts = nodeHtmlParser.parse(response.data)
-    let arrFunctionalGroupOfProducts = resultFunctionalGroupOfProducts
-        .querySelector('.catalog').querySelectorAll('.object')
-    // let contentFunctionalGroupOfProducts = arrFunctionalGroupOfProducts[0].structuredText
-        
-    for(let k = 0; k < arrFunctionalGroupOfProducts.length; ++k){
-        let pageUrlFunctionalGroupOfProducts = arrFunctionalGroupOfProducts[k].querySelector('.header')
-            .getAttribute('href');
-        // console.log(pageUrlFunctionalGroupOfProducts)
-    }
-    // axios.get(baseUrl+pageUrlFunctionalGroupOfProducts).then(function(response){
-    //     const resultProductsGroup = nodeHtmlParser.parse(response.data);
-    //     let arrProductsGroup = resultProductsGroup.querySelector('.catalog')
-    //         .querySelectorAll('.object')
-    //     // for(let j = 0; j < arrProductsGroup.length; ++j){
-    //     let j = 0
-    //     while(j < arrProductsGroup.length){
-    //         let contentProductsGroup = arrProductsGroup[j].structuredText
-    //         let pageUrlProductsGroup = arrProductsGroup[j].querySelector('.header')
-    //             .getAttribute('href')
-    //         axios.get(baseUrl+pageUrlProductsGroup).then(function(response){
-    //             const resultProducts = nodeHtmlParser.parse(response.data)
-    //             let arrSpecifications = resultProducts.querySelectorAll('tr')
-                
-    //             for(let i = 1; i < arrSpecifications.length; ++i){
-    //                 contentProducts = arrSpecifications[i]
-    //                     .structuredText.replace(/\r?\n/g, ";");
-    //                 fs.appendFileSync('some.csv', contentCatalog+';');
-    //                 fs.appendFileSync('some.csv', contentFunctionalGroupOfProducts+';');
-    //                 fs.appendFileSync('some.csv', contentProductsGroup+';');
-
-    //                 fs.appendFileSync('some.csv', contentProducts+';');
-    //                 fs.appendFileSync('some.csv', "\n");
-    //             }
-                
-    //         })
-    //         j++
-    //     }
-    // })
-})
-
-fs.appendFileSync('some.csv', "\n");
